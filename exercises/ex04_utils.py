43124221a57fcccb1fe34509a8f60e449bcf13f2
Owen Bell <bellonb@ad.unc.edu>
2023-10-11 16:19:46 -0400

    ex04 completed commit

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,10 +1,10 @@
-"""Establishing module for list utility functions"""
+"""Establishing module for list utility functions."""
 
 __author__: str = "730656976"
 
+
 def all(input_list: list[int], attempt: int) -> bool:
     """Indicates if all ints in list are equal to the given int."""
-    
     is_same: bool = True
     if len(input_list) == 0:
         return False
@@ -18,20 +18,19 @@ def all(input_list: list[int], attempt: int) -> bool:
 
 def max(input_list: list[int]) -> int:
     """Returns the largest number in a list."""
-
     if len(input_list) == 0:
         raise ValueError("max() arg is an empty List")
     largest: int = input_list[0]
-    for i in range(len(input_list) - 1):
-        if input_list[i] < input_list[i + 1]:
-            largest = input_list[i +1 ]
+    for i in range(len(input_list)):
+        if largest < input_list[i]:
+            largest = input_list[i]
     return largest
 
-def is_equal(lst_one: list[int], lst_two: list[int]) -> bool:
-    """Checks to see if integer lists have the same contents"""
 
+def is_equal(lst_one: list[int], lst_two: list[int]) -> bool:
+    """Checks to see if integer lists have the same contents."""
     if len(lst_one) != len(lst_two):
-        raise ValueError("the lists do not have equal size")
+        return False
     has_same: bool = True
     for i in range(len(lst_one)):
         if lst_one[i] != lst_two[i]:
